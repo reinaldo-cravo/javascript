ceceb0ed50ce2ab8d3a225e269f16307778d688f
Reinaldo Gomes <148367113+reinaldo-cravo@users.noreply.github.com>
2023-10-30 15:33:22 -0300

    js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 100
+console.log(`A velocidadedo seu carro é ${vel}km/h`)
+
+if (vel >= 60) { //condição simples
+    console.log('voce ultrapassou a velocidade permitida. MULTADO.')
+}
+
+console.log('dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil') {
+    console.log('vc eh brasileiro')
+} else {
+    console.log('vc eh estrangeiro')
+}
